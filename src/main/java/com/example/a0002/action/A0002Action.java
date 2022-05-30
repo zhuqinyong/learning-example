4d23c7fe9209bf05dfcdf11a32f47ac50fc9ac4e
zhuqinyong <18326657779@163.com>
2022-05-30 16:04:30 +0800

    修正页面

--- src/main/java/com/example/a0002/action/A0002Action.java
+++ src/main/java/com/example/a0002/action/A0002Action.java
@@ -8,17 +8,12 @@ import javax.swing.*;
 
 public class A0002Action extends AnAction {
 
-    JFrame jFrame = new JFrame("test");
-    A0002UI a0002UI=new A0002UI();
+
 
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
-        jFrame.setContentPane(a0002UI.getRoot());
-        jFrame.setVisible(true);
-        jFrame.setSize(1000,800);
-        jFrame.toFront();
-        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+
     }
 }
--- src/main/java/com/example/a0002/ui/A0002UI.java
+++ src/main/java/com/example/a0002/ui/A0002UI.java
@@ -5,9 +5,20 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class A0002UI extends JFrame {
+
+    private final JFrame jFrame = new JFrame("test");
+    private final A0002UI a0002UI = new A0002UI();
+
+    {
+        jFrame.setContentPane(a0002UI.getRoot());
+        jFrame.setVisible(true);
+        jFrame.setSize(1000, 800);
+        jFrame.toFront();
+        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+    }
+
     private JPanel root;
     private JTable exampleTable;
-
 
     public JPanel getRoot() {
         return root;
